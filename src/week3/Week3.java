207fcfa991c5833bf52655aab931a534944e400d
689tan <689tan@gmail.com>
2023-09-23 11:48:47 -0600

    first commit

---
+++ src/week3/Week3.java
@@ -0,0 +1,27 @@
+package week3;
+
+public class Week3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 12 ;
+int b = 10 ;
+
+
+
+if (a > 2 && b > 2) {
+	int [][] arr = new int[a][b] ;
+	
+	for (int row = 0 ; row < a ; row++) {
+		for (int column = 0 ; column < b ; column++) {
+			arr[row][column] = column;
+			
+		}
+	}
+	System.out.println(arr[0][arr[0].length - 1]);
+}else {
+	System.out.println("Both numbers need to be greater than 2.");
+}
+	}
+
+}
